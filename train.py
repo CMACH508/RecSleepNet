7c01fbf2ddff69093d405428dd99e2bc1a663964
CMACH508 <52200550+CMACH508@users.noreply.github.com>
2021-12-28 13:30:16 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 import numpy as np
 import tensorflow as tf
 from data import get_split
-from model_rec import TinySleepNet
+from model import TinySleepNet
 from minibatching import batch_generator
 
 
@@ -30,8 +30,9 @@ def train(
     best_f1 = 0
     if model.use_rec:
         for epoch in range(config["pretrain_epochs"]):
+            print("pretrain_epoch:{}".format(epoch))
             shuffle_idx = np.random.permutation(np.arange(len(train_x)))
-            train_minibatch_fn = batch_generator(train_x,train_y,config)
+            train_minibatch_fn = batch_generator(train_x,train_y,config,shuffle_idx=shuffle_idx)
             model.train(train_minibatch_fn)
             valid_minibatch_fn = batch_generator(valid_x,valid_y,config)
             model.train(valid_minibatch_fn)
@@ -41,7 +42,7 @@ def train(
     for epoch in range(config["n_epochs"]):
         print("epoch:{}".format(epoch))
         shuffle_idx = np.random.permutation(np.arange(len(train_x)))
-        train_minibatch_fn = batch_generator(train_x,train_y,config)
+        train_minibatch_fn = batch_generator(train_x,train_y,config,shuffle_idx=shuffle_idx)
         train_acc,train_f1,loss = model.train(train_minibatch_fn)
         print("Train: Acc:{:.4f}, F1:{:.4f}, loss:{:.4f}".format(train_acc,train_f1,loss))
         valid_minibatch_fn = batch_generator(valid_x, valid_y, config)
@@ -52,6 +53,5 @@ def train(
         print("Valid: Acc:{:.4f}, F1:{:.4f}".format(test_acc, test_f1))
         if valid_acc > best_acc and valid_f1 > best_f1:
             best_acc = valid_acc
-            best_mf1 = valid_f1
+            best_f1 = valid_f1
             model.save_best_checkpoint(name="best_model")
-
